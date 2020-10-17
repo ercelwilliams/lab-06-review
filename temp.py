1b2f13a3a5fb7cc182a25f005cf7f955c057aea4
ercelwilliams <70540808+ercelwilliams@users.noreply.github.com>
2020-10-17 00:13:12 -0400

    updated

--- temp.py
+++ temp.py
@@ -1,3 +1,4 @@
-tempincelsius = input("Please enter a temperature in celsius: ")  
-tempinfahrenheit = (tempincelsius * (9 / 5) + 32.0  
-print("The temperature is " + tempinfahrenheit + "°F") 
+tempincelsius = input("Please enter a temperature in celsius: ")
+# print(type(tempincelsius))
+tempinfahrenheit = (float(tempincelsius) * (9 / 5)) + 32.0   
+print("The temperature is " + str(tempinfahrenheit) + "°F") 
